d0848b6279fca54d47441f609a2d57f578d2d073
Orçun Çolak <orcun.colak@hazelcast.com>
2024-03-22 12:15:03 +0300

    upgrade spring version

--- src/main/java/com/colak/springschedulertutorial/config/SchedulerConfig.java
+++ src/main/java/com/colak/springschedulertutorial/config/SchedulerConfig.java
@@ -1,7 +1,6 @@
 package com.colak.springschedulertutorial.config;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
